97aafa44b2f9eb120d45566af21d574bb0be9c3a
Debuy <43849498+AppPlusx2@users.noreply.github.com>
2023-05-22 22:25:08 +0800

    Home part 1

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./layout/Header";
+import Home from "./layout/Home";
 
 function Main() {
   return (
     <body className="container">
       <Header />
+      <Home />
     </body>
   );
 }
---
+++ src/components/layout/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home() {
+  return (
+    <header className="header">
+      <h3 className="heading-3">Why Debuy ?</h3>
+      <h1 className="heading-1">Decentralized Contracts</h1>
+      <h5 className="heading-5">
+        Debuy.co is on a mission to revolutionize the procurement industry
+        through the use of web3 technologies. We strive to provide a
+        decentralized and secure platform that empowers businesses and
+        individuals to buy and sell goods and services without the need for
+        intermediaries. Our goal is to foster a more efficient, transparent, and
+        inclusive procurement ecosystem that benefits all participants.
+      </h5>
+    </header>
+  );
+}
+
+export default Home;
